197c9e8269a5f8b353bafba638e2f40bac2c3d24
Ranula Ranatunga <63148540+RanulaRanatunga@users.noreply.github.com>
2024-12-07 16:08:31 +0530

    changes

--- lib/models/menu_model.dart
+++ lib/models/menu_model.dart
@@ -6,32 +6,23 @@
 //
 
 class MenuModel {
-
   final String menuId;
   final String name;
   final List<String> menuCategoryIds;
 
-
   MenuModel({
-
     required this.menuId,
     required this.name,
     required this.menuCategoryIds,
-
   });
 
-
   factory MenuModel.fromJson(Map<String, dynamic> json) {
-
     return MenuModel(
-
       menuId: json['MenuID'] ?? '',
-      name: json['Title']?['en'] ?? '', 
-      menuCategoryIds: json['MenuCategoryIDs'] != null 
-          ? List<String>.from(json['MenuCategoryIDs']) 
+      name: json['Title']?['en'] ?? '',
+      menuCategoryIds: json['MenuCategoryIDs'] != null
+          ? List<String>.from(json['MenuCategoryIDs'])
           : [],
     );
-
   }
-
 }
